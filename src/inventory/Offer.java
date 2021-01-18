6d99c78192ae8f7296782f0b0d7ede0efb25bb4f
Luke Hanna <Luke.m.hanna@gmail.com>
2021-01-18 10:16:12 -0500

    Refactor: Privatized offer variables & Created getters/setters

--- src/inventory/Offer.java
+++ src/inventory/Offer.java
@@ -4,11 +4,11 @@ package inventory;
 // Not all items trade on a 1 to 1 value and some items can't be split so Offers have an amount of input quantity and an amount of output quantity, as well as the items being traded
 public class Offer {
 
-	Itemizable input;	// Item input
-	int inputQuantity;	// Amount of input
+	private Itemizable input;	// Item input
+	private int inputQuantity;	// Amount of input
 	
-	Itemizable output;	// Item output
-	int outputQuantity;	// Amount of output
+	private Itemizable output;	// Item output
+	private int outputQuantity;	// Amount of output
 	
 	// Constructor for standard offer, with arguments passed (quantityX [of] itemX [for] quantityY [of] itemY) 
 	public Offer(int inputQuantity, Itemizable input, int outputQuantity, Itemizable output) {
@@ -16,5 +16,15 @@ public class Offer {
 		this.input = input;						// Item input quantity
 		this.outputQuantity = outputQuantity;	// Item output
 		this.output = output;					// Item output Quantity
+	}
+	
+	// Get method that returns the input item for offer
+	public Itemizable getInput() {
+		return input; // Return the input item
+	}
+	
+	// Get method that returns the output item for offer
+	public Itemizable getOutput() {
+		return output; // Return the output item
 	}
 }
